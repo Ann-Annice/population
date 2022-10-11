66e83f2fce33d8dcf90808e4b380c619e40632d9
Ann-Annice <annannice98@gmail.com>
2022-10-11 21:31:27 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PopulationComponent } from './population/population.component';
+import { RouterModule, Routes } from '@angular/router';
+import{HttpClientModule} from '@angular/common/http';
+const appRoutes:Routes=[
+  {
+    path:"",component:PopulationComponent
+  },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    PopulationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
